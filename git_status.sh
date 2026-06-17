aaac197d1f0ec9dc0c0ac0bb2c5c7e9477c9803e
hariaravind <hariaravind2k8@gmail.com>
2026-06-17 10:45:11 +0000

    the status of files

---
+++ git_status.sh
@@ -0,0 +1,9 @@
+echo "---git repository statistics--"
+echo "1.total number of commits:"
+git rev-list --count HEAD.
+echo "2.number of commits per author"
+git log --format=%an | sort | uniq -c | sort -rn
+echo "3.the recently modified files"
+git log --name-only --format="" | grep -v '^$' | awk '!seen[$0]++' | head -3 
+echo "4.the file that has been changed in the  most commits"
+git log --diff-filter=M --name-only --format="" | grep -v '^$'| sort | uniq -c | sort -rn | head -1
