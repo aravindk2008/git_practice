3c565a511b674048eea2d4ca9bd5d3ae27611438
hariaravind <hariaravind2k8@gmail.com>
2026-06-19 11:55:11 +0000

    Add calculator CI pipeline

---
+++ calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Division by zero is not allowed")
+    return a / b
